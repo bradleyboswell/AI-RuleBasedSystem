9891f6879985b4915338fcf2f72735be02e9cde0
bb05758 <bb05758@georgiasouthern.edu>
2019-10-23 23:21:27 -0400

    blah

--- RuleBasedSystem/RuleBasedSystem/Course.cs
+++ RuleBasedSystem/RuleBasedSystem/Course.cs
@@ -1,22 +1,2 @@
 ﻿using System;
 
-public class Course
-{
-    string name;
-    bool isTaken, fall, spring, summer, onDemand;
-    List<Course> CoReqs, PreReqs, PostReqs;
-
-
-	public Course(string name, bool isTaken, bool fall, bool spring, bool summer, bool onDemand, List<Course> CoReqs, List<Course> PreReqs, List<Course> PostReqs)
-	{
-        this.name = name;
-        this.isTaken = isTaken;
-        this.fall = fall;
-        this.spring = spring;
-        this.summer = summer;
-        this.onDemand = onDemand;
-        this.CoReqs = CoReqs;
-        this.PreReqs = PreReqs;
-        this.PostReqs = PostReqs;
-	}
-}
--- RuleBasedSystem/RuleBasedSystem/Home.cs
+++ RuleBasedSystem/RuleBasedSystem/Home.cs
@@ -16,6 +16,50 @@ namespace RuleBasedSystem
         public Home()
         {
             InitializeComponent();
+            List<Course> CoReqs = new List<Course>();
+            List<Course> PreReqs = new List<Course>();
+            List<Course> PostReqs = new List<Course>();
+            Course comp1 = new Course("ENGL 1101 Composition I", true, true, true, true, true,CoReqs,PreReqs,PostReqs);
+            PrintDialog(comp1.name);
+
         }
+
+        public class Course
+        {
+            string name;
+            bool isTaken, fall, spring, summer, onDemand;
+            List<Course> CoReqs, PreReqs, PostReqs;
+
+
+	        public Course(string name, bool isTaken, bool fall, bool spring, bool summer, bool onDemand, List<Course> CoReqs, List<Course> PreReqs, List<Course> PostReqs)
+	        {
+                this.name = name;
+                this.isTaken = isTaken;
+                this.fall = fall;
+                this.spring = spring;
+                this.summer = summer;
+                this.onDemand = onDemand;
+                this.CoReqs = CoReqs;
+                this.PreReqs = PreReqs;
+                this.PostReqs = PostReqs;
+	        }
+        }
+
+        public class Person
+        {
+             public List<Course> coursesTaken;
+             public bool isFall, isSpring, isSummer;
+
+	        public Person(bool isFall,bool isSpring,bool isSummer)
+	        {
+                this.isFall = isFall;
+                this.isSpring = isSpring;
+                this.isSummer = isSummer;
+                coursesTaken = new List<Course>();
+
+	        }
+        }
+
+
     }
 }
