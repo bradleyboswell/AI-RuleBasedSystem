c6dc7ed8610f01f75d8591a13b3bbb3cf1395c13
bb05758 <bb05758@georgiasouthern.edu>
2019-10-23 23:25:10 -0400

    blah2

--- RuleBasedSystem/RuleBasedSystem/Home.cs
+++ RuleBasedSystem/RuleBasedSystem/Home.cs
@@ -24,42 +24,6 @@ namespace RuleBasedSystem
 
         }
 
-        public class Course
-        {
-            string name;
-            bool isTaken, fall, spring, summer, onDemand;
-            List<Course> CoReqs, PreReqs, PostReqs;
-
-
-	        public Course(string name, bool isTaken, bool fall, bool spring, bool summer, bool onDemand, List<Course> CoReqs, List<Course> PreReqs, List<Course> PostReqs)
-	        {
-                this.name = name;
-                this.isTaken = isTaken;
-                this.fall = fall;
-                this.spring = spring;
-                this.summer = summer;
-                this.onDemand = onDemand;
-                this.CoReqs = CoReqs;
-                this.PreReqs = PreReqs;
-                this.PostReqs = PostReqs;
-	        }
-        }
-
-        public class Person
-        {
-             public List<Course> coursesTaken;
-             public bool isFall, isSpring, isSummer;
-
-	        public Person(bool isFall,bool isSpring,bool isSummer)
-	        {
-                this.isFall = isFall;
-                this.isSpring = isSpring;
-                this.isSummer = isSummer;
-                coursesTaken = new List<Course>();
-
-	        }
-        }
-
 
     }
 }
