81d12b761f69d95f8e390ec45573d48a0c1cf3ff
Brad Boswell <bboswell7194@gmail.com>
2019-10-23 23:08:18 -0400

    Added Person and Course class

--- RuleBasedSystem/RuleBasedSystem/Home.cs
+++ RuleBasedSystem/RuleBasedSystem/Home.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace RuleBasedSystem
 {
+
     public partial class Home : Form
     {
         public Home()
---
+++ RuleBasedSystem/RuleBasedSystem/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Person
+{
+    public List<Course> coursesTaken;
+    public bool isFall, isSpring, isSummer;
+
+	public Person(bool isFall,bool isSpring,bool isSummer)
+	{
+        this.isFall = isFall;
+        this.isSpring = isSpring;
+        this.isSummer = isSummer;
+        coursesTaken = new List<Course>();
+
+	}
+}
