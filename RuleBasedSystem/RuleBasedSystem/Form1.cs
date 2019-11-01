c283605b78c5fc8881ddac3ce1d621f28bea9e50
andrew101sanders <andrew101sanders@gmail.com>
2019-10-31 20:36:42 -0400

    Added all class names

--- RuleBasedSystem/RuleBasedSystem/Form1.cs
+++ RuleBasedSystem/RuleBasedSystem/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -72,48 +73,7 @@ namespace RuleBasedSystem
 
 
             //Course List set to IsCompleted = false by default
-            Course healthful_living = new Course
-            {
-                Prefix = "HLTH 1520",
-                IsCompleted = false,
-                IsAvailable = true,
-                Fall = true,
-                Summer = true,
-                Spring = true,
-                OnDemand = true
-            };
-            Course physical_activity_1 = new Course
-            {
-                Prefix = "KINS 1XXX",
-                IsCompleted = false,
-                IsAvailable = true,
-                Fall = true,
-                Summer = true,
-                Spring = true,
-                OnDemand = true
-            };
-            Course physical_activity_2 = new Course
-            {
-                Prefix = "KINS 1XXX",
-                IsCompleted = false,
-                IsAvailable = true,
-                Fall = true,
-                Summer = true,
-                Spring = true,
-                OnDemand = true
-            };
-            Course first_year_experience = new Course
-            {
-                Prefix = "FYE 1220",
-                IsCompleted = false,
-                IsAvailable = true,
-                Fall = true,
-                Summer = true,
-                Spring = true,
-                OnDemand = true
-            };
-
-            Course engl_1101 = new Course
+            Course Composition_1 = new Course
             {
                 Prefix = "ENGL 1101",
                 IsCompleted = false,
@@ -121,9 +81,10 @@ namespace RuleBasedSystem
                 Fall = true,
                 Summer = true,
                 Spring = true,
-                OnDemand = true
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
             };
-            Course engl_1102 = new Course
+            Course Composition_2 = new Course
             {
                 Prefix = "ENGL 1102",
                 IsCompleted = false,
@@ -131,9 +92,10 @@ namespace RuleBasedSystem
                 Fall = true,
                 Summer = true,
                 Spring = true,
-                OnDemand = true
+                OnDemand = true,
+                Prereqs = new Course[,] { { Composition_1 } }
             };
-            Course college_algebra = new Course
+            Course College_Algebra = new Course
             {
                 Prefix = "MATH 1111",
                 IsCompleted = false,
@@ -141,9 +103,10 @@ namespace RuleBasedSystem
                 Fall = true,
                 Summer = true,
                 Spring = true,
-                OnDemand = true
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
             };
-            Course trigonometry = new Course
+            Course Trigonometry = new Course
             {
                 Prefix = "MATH 1112",
                 IsCompleted = false,
@@ -151,9 +114,10 @@ namespace RuleBasedSystem
                 Fall = true,
                 Summer = true,
                 Spring = true,
-                OnDemand = true
+                OnDemand = true,
+                Prereqs = new Course[,] { { College_Algebra } }
             };
-            Course pre_calculus = new Course
+            Course Pre_Calculus = new Course
             {
                 Prefix = "MATH 1113",
                 IsCompleted = false,
@@ -161,9 +125,10 @@ namespace RuleBasedSystem
                 Fall = true,
                 Summer = true,
                 Spring = true,
-                OnDemand = true
+                OnDemand = true,
+                Prereqs = new Course[,] { { Trigonometry } }
             };
-            Course calculus = new Course
+            Course Calculus_1 = new Course
             {
                 Prefix = "MATH 1441",
                 IsCompleted = false,
@@ -171,9 +136,10 @@ namespace RuleBasedSystem
                 Fall = true,
                 Summer = true,
                 Spring = true,
-                OnDemand = true
+                OnDemand = true,
+                Prereqs = new Course[,] { { Pre_Calculus, Trigonometry } }
             };
-            Course world_history_2 = new Course
+            Course World_History_2 = new Course
             {
                 Prefix = "HIST 1112",
                 IsCompleted = false,
@@ -181,20 +147,387 @@ namespace RuleBasedSystem
                 Fall = true,
                 Summer = true,
                 Spring = true,
-                OnDemand = true
+                OnDemand = true,
+                Prereqs = new Course[,] { {  } }
             };
-            
 
+            Course Gloabl_Citizens = new Course
+            {
+                Prefix = "FYE 1410",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course World_Literature_1_or_2 = new Course
+            {
+                Prefix = "ENGL 2111/2112",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { Composition_2 } }
+            };
+
+            Course Public_Speaking = new Course
+            {
+                Prefix = "COMM 1110",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { Composition_1 } }
+            };
+
+            Course Principles_of_Chemistry_1_with_lab = new Course
+            {
+                Prefix = "CHEM 1211K",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { {  } }
+            };
+
+            Course Introduction_to_the_Earth = new Course
+            {
+                Prefix = "GEOL 1121",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Introduction_to_Physics_1 = new Course
+            {
+                Prefix = "PHYS 1111 K",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { Trigonometry, Pre_Calculus } }
+            };
+
+            Course Principles_of_Physics_1 = new Course
+            {
+                Prefix = "PHYS 2211K",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { Calculus_1 } }
+            };
+
+            Course Environmental_Biology_with_lab = new Course
+            {
+                Prefix = "BIOL 1230 & 1210",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { {  } }
+            };
+
+            Course Chemistry_and_the_Environment_with_lab = new Course
+            {
+                Prefix = "CHEM 1040",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Environment_Geology_with_lab = new Course
+            {
+                Prefix = "GEOL 1340",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Environment_Physics_with_lab = new Course
+            {
+                Prefix = "PHYS 1149",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Economics_in_a_Global_Society = new Course
+            {
+                Prefix = "ECON 2105",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course US_A_Comprehensive_Survey = new Course
+            {
+                Prefix = "HIST 2110",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course American_Government = new Course
+            {
+                Prefix = "POLS 1101",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Introduction_to_Anthropology = new Course
+            {
+                Prefix = "ANTH 1102",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course World_Regional_Geography = new Course
+            {
+                Prefix = "GEOG 1130",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Introduction_to_Phychology = new Course
+            {
+                Prefix = "PSYC 1101",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Introduction_to_Sociology = new Course
+            {
+                Prefix = "SOCI 1101",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Healthful_Living = new Course
+            {
+                Prefix = "HLTH 1520",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Physical_Activity_1 = new Course
+            {
+                Prefix = "KINS 1XXX",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course Physical_Activity_2 = new Course
+            {
+                Prefix = "KINS 1XXX",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
+
+            Course First_Year_Experience = new Course
+            {
+                Prefix = "FYE 1220",
+                IsCompleted = false,
+                IsAvailable = true,
+                Fall = true,
+                Summer = true,
+                Spring = true,
+                OnDemand = true,
+                Prereqs = new Course[,] { { } }
+            };
 
             List<Course> courses = new List<Course>
             {
-                engl_1101,
-                engl_1102,
-                college_algebra,
-                trigonometry,
-                pre_calculus,
-                calculus
+                Composition_1,
+                Composition_2,
+
+                College_Algebra,
+                Trigonometry,
+                Pre_Calculus,
+                Calculus_1,
+
+                World_History_2,
+                Gloabl_Citizens,
+
+                World_Literature_1_or_2,
+                Public_Speaking,
+
+                Principles_of_Chemistry_1_with_lab,
+                Introduction_to_the_Earth,
+                Introduction_to_Physics_1,
+                Principles_of_Physics_1,
+
+                Environmental_Biology_with_lab,
+                Chemistry_and_the_Environment_with_lab,
+                Environment_Geology_with_lab,
+                Environment_Physics_with_lab,
+
+                Economics_in_a_Global_Society,
+                US_A_Comprehensive_Survey,
+                American_Government,
+
+                Introduction_to_Anthropology,
+                World_Regional_Geography,
+                Introduction_to_Phychology,
+                Introduction_to_Sociology,
+
+                Healthful_Living,
+                Physical_Activity_1,
+                Physical_Activity_2,
+                First_Year_Experience,
+
+                Programming_Principles_1,
+                Programming_Principles_2,
+                Computers_Ethics_and_Society,
+                Discrete_Math,
+                Calculus_2,
+                Statistics_1,
+
+                Introduction_to_International_Studies,
+
+                Principles_of_Biology_2_with_lab,
+                Principles_of_Chemistry_2_with_lab,
+                General_Historal_Geology_with_lab,
+                Introduction_to_Physics_2_with_lab,
+                Principles_of_Physics_2,
+
+                Data_Structures,
+                Systems_Software,
+                Theoretical_Foundations,
+                Database_Systems,
+                Algorithm_Design_and_Analysis,
+                Computer_Architecture,
+                Data_Comm_and_Networking,
+                Object_Oriented_Design,
+                Computer_Security,
+                Distributed_Web_Systems_Design,
+                Software_Engineering,
+
+                Introduction_to_Computer_Engineering,
+                Logic_Circuits_and_Microprocessors,
+                Data_Warehouse_Design,
+                Human_Computer_Interaction,
+                Game_Programming,
+                Software_Testing_and_Quality_Assurance,
+                Broadband_Networks,
+                Optical_Networks,
+                Selected_Topics,
+                Discrete_Simulation,
+                Artificial_Inteliigence,
+                Computer_Graphics,
+                Animation,
+                Systems_and_Software_Assurance,
+                Network_Management_Systems,
+                Wireless_and_Mobile_Systems,
+
+                Introduction_to_Operating_System,
+                Comparative_Languages,
+                Machine_Learning,
+
+                //https://catalog.georgiasouthern.edu/academics/course-descriptions/csci/
+                Comp_App_For_Bus_Majors,
+                Introduction_to_BASIC_Programming,
+                Introduction_to_Java_Programming,
+                Cpp_Programming,
+                High_Performance_Computing,
+                Networks,
+                Advanced_Database_Systems,
+                Advanced_Operating_Systems,
+                System_Prog_Under_Unix,
+                Compiler_Theory,
+                Embedded_Systems_Programming,
+                Handheld_Ubiquitous_Computing,
+                Numerical_Analysis,
+                Data_Mining,
+                Special_Problems_CO-OP,
+                Directed_Study_in_Computer_Science,
+                Data_Management_for_Math_and_the_Sciences,
+                Discrete_Simulation,
+                Software_Security_and_Secure_Coding
+
+                
+
             };
+
 
             if (NextSpring)
             {
@@ -217,7 +550,6 @@ namespace RuleBasedSystem
 
                 });
             }
-
         }
 
         public static List<Func<T, bool>> CompileRuleSet<T>(List<T> targetSet, List<Rule> ruleSet)
@@ -233,7 +565,7 @@ namespace RuleBasedSystem
                 //Console.WriteLine("r.Source = " + r.Source);
                 //Console.WriteLine("MemberExpression srcIdent = " + srcIdent);
 
-                PropertyInfo srcPropInfo = typeof(T).GetProperty(r.Source);              //Get the property info object about the source arg for this rul
+                PropertyInfo srcPropInfo = typeof(T).GetProperty(r.Source);              //Get the property info object about the source arg for this rule
                 Type propType = srcPropInfo.PropertyType;                                   //gets property type associated with source arg for this rule
                //Console.WriteLine("Type propType = " + propType.Name);
 
@@ -252,8 +584,6 @@ namespace RuleBasedSystem
 
             return compiledOut;         //return set of compiled rules
         }
-
-      
 
         public class Course
         {
@@ -303,6 +633,14 @@ namespace RuleBasedSystem
                 set;
             }
 
+            //Outer elements are ANDed together, inner elements are ORed together
+            public Course[,] Prereqs
+            {
+                get;
+                set;
+            }
+
+
             //To string method for returning info to the user about the status of this course
             public new string ToString()
             {
@@ -315,9 +653,7 @@ namespace RuleBasedSystem
                     return Prefix + " has either not been taken, or not been passed with a C or better";
                 }
             }
-
         }
-
 
         public class Rule
         {
